aa498f57539fa4d70e0575344f6e7fd3642fbaf9
Sravanthi Rani Sinha <37@p6-2.hbtn.io>
2016-05-25 23:04:31 -0700

    task 1 finsihsed

--- run_your_first_ios_app/TapperProject/TapperProject/ViewController.swift
+++ run_your_first_ios_app/TapperProject/TapperProject/ViewController.swift
@@ -9,6 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var image_tapper: UIImageView!
+   
+    @IBOutlet weak var button_play: UIButton!
+    
+    
+    @IBOutlet weak var textfield_number: UITextField!
+    
+
+        
+    @IBAction func clickPlayButton(sender: UIButton) {
+    
+        let num = Int(textfield_number?.text ?? "")
+        
+        if num != nil {
+            if num >= 0 {
+            print("Let's do "+String(num)+" taps")
+            }
+        }
+    
+    }
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,7 +40,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-}
+ }
 
