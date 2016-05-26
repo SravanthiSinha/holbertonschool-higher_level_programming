f06837e21be52385b08cfcbf23a3fa18019d5cff
Sravanthi Rani Sinha <37@p6-2.hbtn.io>
2016-05-25 18:30:34 -0700

    finsihed task

---
+++ hodor/level_1/vote.py
@@ -0,0 +1,18 @@
+import mechanize #sudo pip install python-mechanize
+
+br = mechanize.Browser() #initiating a browser
+
+br.set_handle_robots(False) #ignore robots.txt
+
+br.addheaders = [("User-agent","Mozilla/5.0")] #our identity
+
+for x in range(0,1024):
+        hsbot = br.open("http://173.246.108.142/level1.php")
+
+        br.select_form(nr=0) #the sign up form in github is in third position(search and sign in
+
+        br["id"] = '37' #username for github
+
+        sign_up = br.submit()
+        print x
+                        
