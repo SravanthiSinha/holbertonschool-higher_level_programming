aea45651751bac0bf4b68dffb074a64a93aff698
Sravanthi Rani Sinha <37@p6-2.hbtn.io>
2016-05-27 00:00:54 -0700

    finished project

---
+++ welcome_to_silicon_valley/SiliconValleyCompanies/SiliconValleyCompanies/Models/TechCompaniesHelper.swift
@@ -0,0 +1,19 @@
+//
+//  TechCompaniesHelper.swift
+//  
+//
+//  Created by Sravanthi Rani Sinha on 5/26/16.
+//
+//
+
+import Foundation
+
+class TechCompaniesHelper{
+    
+static var companies:[String] = ["Holberton", "Linkedin", "Docker", "Google", "Yahoo", "Apple"]
+
+static func getTechCompanies()->[String]{
+    return companies
+}
+    
+}
