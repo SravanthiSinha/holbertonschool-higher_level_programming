5b740125d523891e61a228ae3bcd3964f7a2138d
sravanthisinha <sravani.sinha@gmail.com>
2017-04-21 14:49:39 -0700

    fixed the doctest for 102

--- 0x07-python-classes/102-square.py
+++ 0x07-python-classes/102-square.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
 """
-Square class with basic printing function
+This is the Square module.
+This is a Square class inside the square module.
 """
 
 
 class Square:
-    def __init__(self, size=0, position=(0, 0)):
+    """
+    This is a Square class
+    """
+    def __init__(self, size=0):
         """
-        initialize square with size and pos
+        Initializes the square and the __size
         """
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
-        errormsg = 'position must be a tuple of 2 positive integers'
-        if type(position) is not tuple:
-            raise TypeError(errormsg)
-        if len(position) != 2:
-            raise TypeError(errormsg)
-        for i in position:
-            if type(i) is not int or i < 0:
-                raise TypeError(errormsg)
         self.__size = size
-        self.__position = position
 
     def area(self):
+        """
+        Returns the current square area
+        """
         return (self.__size ** 2)
 
     @property
@@ -36,40 +34,8 @@ class Square:
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if value < 0:
-            raise ValueError('size must be >=0')
+            raise ValueError('size must be >= 0')
         self.__size = value
-
-    @property
-    def position(self):
-        return (self.__position)
-
-    @size.setter
-    def size(self, value):
-        errormsg = 'position must be a tuple of 2 positive integers'
-        if type(value) is not tuple:
-            raise TypeError(errormsg)
-        if len(value) != 2:
-            raise TypeError(errormsg)
-        for i in value:
-            if type(i) is not int or i < 0:
-                raise TypeError(errormsg)
-        self.__position = value
-
-    def my_print(self):
-        if self.__size == 0:
-            print("")
-        else:
-            print("\n" * self.__position[1], end="")
-            print("\n".join([" " * self.position[0] + "#" * self.__size
-                             for i in range(self.__size)]))
-
-    def __str__(self):
-        """ does this need this """
-        if self.__size == 0:
-            return("")
-        return(("\n" * self.__position[1]) +
-               ("\n".join((" " * self.__position[0]) +
-                          "#" * self.__size for i in range(self.__size))))
 
     def __gt__(self,other):
         return self.area() > other.area()
