2279c5fccf00c291b6db70d22c939b4b97b6e81c
sravanthisinha <sravani.sinha@gmail.com>
2017-04-21 16:36:50 -0700

    task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is the Rectangle module.
+This is a Rectangle class inside the Rectangle module.
+"""
+
+
+class Rectangle:
+    """
+    This is a empty Rectangle class
+    """
+    pass
