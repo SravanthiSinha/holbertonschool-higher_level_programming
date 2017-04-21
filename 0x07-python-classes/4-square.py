9db79e28bafc994910ed86631602c29218cebb53
sravanthisinha <sravani.sinha@gmail.com>
2017-04-21 12:17:54 -0700

    fixing the doctest

--- 0x07-python-classes/4-square.py
+++ 0x07-python-classes/4-square.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
 """
-Square class
+This is the Square module.
+This is a Square class inside the square module.
 """
 
+
 class Square:
+    """
+    This is a Square class
+    """
     def __init__(self, size=0):
+        """
+        Initializes the square and the __size
+        """
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
-            raise ValueError('size must be >=0')
+            raise ValueError('size must be >= 0')
         self.__size = size
 
     def area(self):
+        """
+        Returns the current square area
+        """
         return (self.__size ** 2)
 
     @property
@@ -23,5 +34,5 @@ class Square:
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if value < 0:
-            raise ValueError('size must be >=0')
+            raise ValueError('size must be >= 0')
         self.__size = value
