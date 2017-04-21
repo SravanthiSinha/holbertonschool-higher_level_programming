a0b4689e0f029ece56dde12c0b50d99b1b90e1fe
sravanthisinha <sravani.sinha@gmail.com>
2017-04-20 17:10:55 -0700

    finish task 1

---
+++ 0x07-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is the Square module.
+This is a Square class inside the square module.
+"""
+
+
+class Square:
+    """
+    This is a square class
+    """
+    def __init__(self, size):
+        """
+        Initializes the square and the __size and name attributes
+        """
+        self.__size = size
