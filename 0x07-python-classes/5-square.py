08cae8aac000e434a1101403181fa9271f58863d
sravanthisinha <sravani.sinha@gmail.com>
2017-04-20 20:34:46 -0700

    finish task 5

---
+++ 0x07-python-classes/5-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+This is the Square module.
+This is a Square class inside the square module.
+"""
+
+
+class Square:
+    """
+    This is a Square class
+    """
+    def __init__(self, size=0):
+        """
+        Initializes the square and the size
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        returns the size of square
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        sets the size of square
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+
+    def area(self):
+        """
+        returns the current square area
+        """
+        return (self.__size * self.__size)
+
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                print(self.__size * "#")
