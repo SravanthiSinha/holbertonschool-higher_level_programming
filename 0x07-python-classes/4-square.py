76a8f0785e0f20cca35f4ec568aed6ad288d0edb
sravanthisinha <sravani.sinha@gmail.com>
2017-04-20 19:55:10 -0700

    fixing for checks

--- 0x07-python-classes/4-square.py
+++ 0x07-python-classes/4-square.py
@@ -9,27 +9,17 @@ Method area returns size of area of the square.
 
 
 class Square:
-    """
-    This is a Square class
-    """
+    """This is a Square class"""
     def __init__(self, size=0):
-        """
-        Initializes the square and the size
-        """
+        """Initializes the square and the size"""
         self.size = size
 
     @property
     def size(self):
-        """
-        returns the size of square
-        """
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        """
-        sets the size of square
-        """
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -38,7 +28,4 @@ class Square:
 
 
     def area(self):
-        """
-        returns the current square area
-        """
         return (self.__size * self.__size)
